7b2e59a47364eb44e6892aae68dd596ba5712c2b
Marketing del Futuro <122307296+MarketingDF@users.noreply.github.com>
2023-01-24 17:32:06 -0300

    db false

--- api/src/testingCreators/precios.js
+++ api/src/testingCreators/precios.js
@@ -1,10 +1,10 @@
 const { Precio } = require('../db');
 
 function createPrecios() {
-    Precio.create({ name: "GAS NORMAL 5 KILOS", precio: 11850 }); // 5kg
-    Precio.create({ name: "GAS NORMAL 11 KILOS", precio: 18450 }); // 11kg
-    Precio.create({ name: "GAS NORMAL 15 KILOS", precio: 25250 }); // 15kg
-    Precio.create({ name: "GAS NORMAL 45 KILOS", precio: 74250 }); // 45kg
+    Precio.create({ name: "GAS NORMAL 5 KILOS", precio: 11250 }); // 5kg
+    Precio.create({ name: "GAS NORMAL 11 KILOS", precio: 17200 }); // 11kg
+    Precio.create({ name: "GAS NORMAL 15 KILOS", precio: 23950 }); // 15kg
+    Precio.create({ name: "GAS NORMAL 45 KILOS", precio: 71400 }); // 45kg
 }
 
 module.exports = createPrecios
