a9a8e8c8c5285f6fe3f4e7fa754456b87e6ae175
Marketing del Futuro <122307296+MarketingDF@users.noreply.github.com>
2023-01-26 17:28:30 -0300

    patentes

--- api/src/testingCreators/patentes.js
+++ api/src/testingCreators/patentes.js
@@ -12,7 +12,23 @@ const createPatentes = () => {
     Patentes.create({ name: 'LZGB82'});
     Patentes.create({ name: 'LYHR97'});
     Patentes.create({ name: 'PBKB89'});
-    Patentes.create({ name: 'PBKB90'});
+    Patentes.create({ name: 'PBKB91'});
+    Patentes.create({ name: 'PBKB92'});
+    Patentes.create({ name: 'PBKB93'});
+    Patentes.create({ name: 'PBKB94'});
+    Patentes.create({ name: 'PBKB95'});
+    Patentes.create({ name: 'PBKB96'});
+    Patentes.create({ name: 'PBKB97'});
+    Patentes.create({ name: 'PBKB98'});
+    Patentes.create({ name: 'PBKB99'});
+    Patentes.create({ name: 'PBKB10'});
+    Patentes.create({ name: 'PBKB11'});
+    Patentes.create({ name: 'PBKB12'});
+    Patentes.create({ name: 'PBKB13'});
+    Patentes.create({ name: 'PBKB14'});
+    Patentes.create({ name: 'PBKB15'});
+    Patentes.create({ name: 'PBKB16'});
+    Patentes.create({ name: 'PBKB18'});
 };
 
 module.exports = createPatentes
