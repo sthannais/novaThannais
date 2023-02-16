6919d14fa3f081767f08a724347893b66bba4a40
Marketing del Futuro <122307296+MarketingDF@users.noreply.github.com>
2023-02-16 11:56:56 -0300

    lista de precios actualizada

--- api/src/testingCreators/listaDePrecios.js
+++ api/src/testingCreators/listaDePrecios.js
@@ -2,11 +2,11 @@ const { ListaDePrecios } = require('../db');
 
 function createPrecios() {
     ListaDePrecios.create({ 
-        name: "Lista 1ra semana febrero 2023", 
-        precio5kg: 11650,
-        precio11kg: 17600,
-        precio15kg: 24500,
-        precio45kg: 72950,
+        name: "Lista 3ra semana febrero 2023", 
+        precio5kg: 11450,
+        precio11kg: 17200,
+        precio15kg: 24000,
+        precio45kg: 71350,
         active: true
     });
 }
